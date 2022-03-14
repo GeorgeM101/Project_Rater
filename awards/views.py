a2e55814ccbb100e8df2aaf1124befac36262f57
GeorgeM101 <georgmboya@gmail.com>
2022-03-14 16:01:00 +0300

    added home view function

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'home.html', {'projects'}:projects)
