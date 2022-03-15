b16728763c5d1217d1bec094f8d682b9a7f9d657
GeorgeM101 <georgmboya@gmail.com>
2022-03-15 06:48:48 +0300

    added home page

--- awards/models.py
+++ awards/models.py
@@ -9,3 +9,9 @@ class Profile(models.Model):
     name = models.CharField(max_length=30, blank=True)
     email = models.EmailField()
     bio = models.CharField(max_length=500)
+
+    def save_profile(self):
+        self.save()
+    
+    def __str__(self):
+        return self.name
--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
+from .models import Project 
 
 # Create your views here.
 def home(request):
     projects = Project.objects.all()
-    return render(request, 'home.html', {'projects'}:projects)
+    return render(request, 'home.html', {'projects':projects})
