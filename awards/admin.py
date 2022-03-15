4ac6a56f20abc1dfa150778517b37f9d9f775d55
GeorgeM101 <georgmboya@gmail.com>
2022-03-15 13:16:24 +0300

    registered models

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from awards.models import User,Project,Rating,Profile
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Rating)
+admin.site.register(Project)
+admin.site.register(Profile)
