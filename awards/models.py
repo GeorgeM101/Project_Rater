abd7c1d89478f39866a8e3535a242c88063f14f3
GeorgeM101 <georgmboya@gmail.com>
2022-03-15 11:39:05 +0300

    added rating class

--- awards/models.py
+++ awards/models.py
@@ -1,4 +1,3 @@
-from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ImageField
@@ -18,8 +17,31 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def search_by_name(cls,search_term):
+        '''
+        method to search projects based on name
+        '''
+        projects=cls.objects.filter(name__icontains=search_term)
+
+        return projects
+
 class Project(models.Model):
-    title = models.TextField()
-    image = models.ImageField(upload_to = 'proj/')
-    description = models.CharField(max_length=60, null=True)
-    user = models.ForeignKey('Users', on_delete=models.CASCADE, null=True)
+    webimage= models.ImageField(upload_to='webimage/',null=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True)
+    name= models.CharField(max_length=70)
+    description= models.TextField()
+    link= models.CharField(max_length=200)
+
+
+    def save_project(self):
+        self.save()
+
+class Rating(models.Model):
+    design = models.IntegerField(blank=True,default=0)
+    usability = models.IntegerField(blank=True,default=0)
+    creativity = models.IntegerField(blank=True,default=0)
+    content = models.IntegerField(blank=True,default=0)
+    overall_score = models.IntegerField(blank=True,default=0)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
