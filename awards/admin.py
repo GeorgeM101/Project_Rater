0d3395ca9e946d564fb277bab70cc579fa1f7cf7
GeorgeM101 <georgmboya@gmail.com>
2022-03-15 13:29:40 +0300

    added page content

--- awards/admin.py
+++ awards/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from awards.models import User,Project,Rating,Profile
+from awards.models import Project,Rating,Profile
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Rating)
 admin.site.register(Project)
 admin.site.register(Profile)
--- awards/urls.py
+++ awards/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('register',views.register, name='register')
 ]
