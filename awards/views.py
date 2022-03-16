4cb9e576aec20b819c7a407157058293e37987de
GeorgeM101 <georgmboya@gmail.com>
2022-03-16 10:45:41 +0300

    added registration

--- awards/views.py
+++ awards/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render,redirect
-from .models import Project
+from .models import Project,User
+from django.contrib.auth.hashers import make_password
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 def index(request):
     projects = Project.objects.all()
     return render(request, 'home.html', {'projects':projects})
+
+def register(request):
+    if request.method=="POST":
+        phone=request.POST.get('phone')
+        email=request.POST.get('email')
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        confirm_password=request.POST.get('confirm_password')
+        e_username=User.objects.filter(username=username).count()
+        email_exist=User.objects.filter(email=email).count()
+        if e_username>0:
+            messages.add_message(request, messages.ERROR, 'Username exist')
+            return redirect(register)
+        elif email_exist>0:
+            messages.add_message(request, messages.ERROR, 'Email exist')
+            return redirect(register)
+        else:
+            if password!=confirm_password:
+                messages.add_message(request, messages.ERROR, 'Username exist')
+                return redirect(register)
+            else:
+                user = User(username=username, email=email,phone_number=phone,password=make_password(password))
+                user.save()
+                messages.add_message(request, messages.SUCCESS, 'Saved successfully!')
+                return redirect(user_login)
+    else:
+        return render(request, "wards/register.html")
